374ee6065fb54429be53c07043f63ed4ae68ab04
Rahul Ravi <68689057+rahulr2003@users.noreply.github.com>
2021-05-26 11:33:35 +0530

    Create Excercise3.py

---
+++ Excercise3.py
@@ -0,0 +1,8 @@
+"""
+if statements are checks on wether a certain condition is met or not and based on the outcome of the check, a certain code fragment is executed
+
+for loops are iterates a set of processes a certain number of times specified.
+
+using a for loop, print all even integers between 1 and 2000 which are a multiple of 3 and 5.
+   
+"""
