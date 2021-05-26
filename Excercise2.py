399eae7c93b9f127832763a73aef6df765f82d37
Rahul Ravi <68689057+rahulr2003@users.noreply.github.com>
2021-05-26 15:28:57 +0530

    Update Excercise2.py

--- Excercise2.py
+++ Excercise2.py
@@ -3,10 +3,14 @@ Count the number of integers in list formed below starting with 1,2,3,4,5,6,7,8 
 
 Use Minimal lines of code to make the program efficient.
 
-you can use secrets by using code : import secrets
+the total number of integers in that list has to be 100000 and the range: 0 to 1000000000
 
-to get a random integer between 0 and n using secrets, write secrets.randbelow(n)
+the code to obtain the list of integers are given below:
 
-the total number of integers in that list has to be 1000000 and the range in which you have to select those random numbers is 0 to 1000000000
+list1 = []
+import random
+for i in range(0,100000):
+   list1.append(random.randint(0,1000000000)
+   
 """
 
