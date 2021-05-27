e4a7b39882b34f8757d5d21952af23e4823e38ab
Rahul Ravi <68689057+rahulr2003@users.noreply.github.com>
2021-05-27 14:17:10 +0530

    Update Excercise2.py

--- Excercise2.py
+++ Excercise2.py
@@ -1,9 +1,11 @@
 """
-Count the number of integers in list formed below starting with 1,2,3,4,5,6,7,8 and 9 respectively.
+Count the number of integers in a list, starting with 1,2,3,4,5,6,7,8 and 9 respectively.
+
+the total number of integers in that list is 100000 and the range: 0 to 1000000000
+
+Store all the counters in a suitable data structure
 
 Use Minimal lines of code to make the program efficient.
-
-the total number of integers in that list has to be 100000 and the range: 0 to 1000000000
 
 the code to obtain the list of integers are given below:
 
